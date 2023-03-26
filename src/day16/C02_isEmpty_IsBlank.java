824db448a9f605363198ba91a992fb7e5b450a38
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-03-26 11:16:41 +0300

    26/03/2023

---
+++ src/day16/C02_isEmpty_IsBlank.java
@@ -0,0 +1,27 @@
+package day16;
+
+public class C02_isEmpty_IsBlank {
+    public static void main(String[] args) {
+        String str = "Java is the life";
+
+        System.out.println(str.isEmpty());//false
+        System.out.println(str.isBlank());//false
+
+        String str2 = "       ";
+        System.out.println(str2.isEmpty());//false
+        System.out.println(str2.isBlank());//true
+
+        String str3 = "";
+        System.out.println(str3.isEmpty());//True
+        System.out.println(str3.isBlank());//true
+
+
+
+        System.out.println(str.length());//16
+        System.out.println(str2.length());//7
+        System.out.println(str3.length());//0
+
+
+    }
+
+}
---
+++ src/day16/C07_Repeat.java
@@ -0,0 +1,10 @@
+package day16;
+
+public class C07_Repeat {
+    public static void main(String[] args) {
+
+        String str = "JAVA *";
+
+        System.out.println(str.repeat(1000));
+    }
+}

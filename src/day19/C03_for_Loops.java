d338a6d39cb06f9084e396ffac65afcd04ad02f0
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-04-01 13:24:35 +0300

    01/04/2023

--- src/day19/C03_for_Loops.java
+++ src/day19/C03_for_Loops.java
@@ -1,8 +1,25 @@
 package day19;
 
+import java.util.Scanner;
+
 public class C03_for_Loops {
     public static void main(String[] args) {
         //Question 7- Get a positive integer from the user and print the sum of the digits.
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int num = scan.nextInt();
+
+        int sum = 0 ;
+
+        for(int i = num; i!=0; num = num/10){
+
+            i = num%10;
+            sum = sum + i;
+
+        }
+
+        System.out.println(sum);
+
 
     }
 }
--- src/day19/C04_for_Loops.java
+++ src/day19/C04_for_Loops.java
@@ -3,7 +3,16 @@ package day19;
 public class C04_for_Loops {
     public static void main(String[] args) {
         //Question 2: Collect and print the String prices given by the user in a certain format.
-
         //input1: "15.31 $" , input2: "11.40 $"   total     //output : 26.71 $
+        String str1="$15.31";
+        String str2="$11.40";
+
+        str1=str1.replaceAll("\\D","");
+        double price1=Double.parseDouble(str1);
+        str2=str2.replaceAll("\\D","");
+        double price2=Double.parseDouble(str2);
+
+        System.out.println("$"+(price1+price2)/100);
+
     }
 }

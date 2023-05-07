ee431ee83bbc6c4773e97ce403ec6c3bbc33de77
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-05-07 14:05:38 +0300

    07/05/2023

---
+++ src/day39_inheratance/YCats.java
@@ -0,0 +1,8 @@
+package day39_inheratance;
+
+public class YCats extends Mammals{
+    String spec = "Always fall on top four feet";
+    void catSound(){
+        System.out.println("Cats meow......");
+    }
+}
